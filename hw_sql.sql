4bcf6abdd01c46d812bb71406ff773982fc20db3
Vladislav Mashin <mashinvladislav@gmail.com>
2022-11-25 11:31:11 +0300

    second commit

--- hw_sql.sql
+++ hw_sql.sql
@@ -1,6 +1,6 @@
 create table if not exists Genre (
 id serial primary key,
-name varchar(60) not null
+name varchar(60) unique not null
 );
 
 create table if not exists Performer (
@@ -17,7 +17,7 @@ constraint gp primary key (genre_id, performer_id)
 create table if not exists Album (
 id serial primary key,
 name varchar(60) not null,
-year date
+YEAR RELEASE integer UNIQUE NOT NULL,
 );
 
 create table if not exists AlbumPerformer (
@@ -26,16 +26,24 @@ performer_id integer references Performer(id),
 constraint ap primary key (album_id, performer_id)
 );
 
+CREATE TABLE IF NOT EXISTS TrackAlbum (
+id serial PRIMARY KEY,
+album_id integer REFERENCES Album(id),
+track_id integer REFERENCES Track(id),
+CONSTRAINT att PRIMARY KEY (album_id, track_id)
+);
+
 create table if not exists Track (
 id serial primary key,
 name varchar(60) not null,
-duration varchar(60) not null
+duration integer not NULL,
+album_id integer REFERENCES Album(id)
 );
 
 create table if not exists Collection (
 id serial primary key,
 name varchar(60) not null,
-year date
+YEAR RELEASE integer UNIQUE NOT NULL
 );
 
 create table if not exists Composition (
